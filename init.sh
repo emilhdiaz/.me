f4c489234d3798d777dd1beedead00ae79791bc1
Emil Diaz <emil.h.diaz@gmail.com>
2018-11-26 12:41:12 -0500

    Updating init commands to match new directory structure

--- init.sh
+++ init.sh
@@ -1,10 +1,7 @@
 cd $HOME;
-ln -s $HOME/.me/.profile .profile;
-ln -s $HOME/.me/.bashrc .bashrc;
-ln -s $HOME/.me/.bash_aliases .bash_aliases;
+ln -s $HOME/.me/profile .profile;
+ln -s $HOME/.me/bashrc .bashrc;
 
+ln -s $HOME/.me/git/gitconfig .gitconfig;
+ln -s $HOME/.me/git/gitignore_global .gitignore_global;
 
-ln $HOME/.me/.gitconfig .gitconfig;
-ln $HOME/.me/.gitignore_global .gitignore_global;
-
-ln $HOME/.me/.ssh/config .ssh/config;
